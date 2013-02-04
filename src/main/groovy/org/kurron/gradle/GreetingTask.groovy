3cdb1746f86895921c95106c101329837b7eb5f1
Ronald D. Kurr <kurr@kurron.org>
2013-02-04 11:50:46 -0500

    Was able to import and use the custom task.

--- src/main/groovy/org/kurron/gradle/GreetingTask.groovy
+++ src/main/groovy/org/kurron/gradle/GreetingTask.groovy
@@ -1,11 +1,12 @@
 package org.kurron.gradle
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 /**
  * An example of creating a configurable task.
  */
-class GreetingTask {
+class GreetingTask extends DefaultTask {
     String greeting = 'default greeting message'
 
     @TaskAction
