377a1c3183dd0804a774992f0018f4d41943955a
Ronald D. Kurr <kurr@kurron.org>
2013-02-05 08:25:50 -0500

    Improved the logging.

--- src/main/groovy/org/kurron/gradle/XjcTask.groovy
+++ src/main/groovy/org/kurron/gradle/XjcTask.groovy
@@ -14,35 +14,43 @@ class XjcTask extends DefaultTask {
     String packageName = 'org.kurron.generated'
     String schemaFile = null
 
-    /*
-     inputs.dir schemaDirectory
-     outputs.dir destination
-     */
-
     @TaskAction
     def work() {
-        if ( null == sourceDirectory )
-        {
-            sourceDirectory = project.sourceSets['main'].resources.srcDirs.toList().first().path
-        }
-        if ( null == destinationDirectory )
-        {
-            destinationDirectory = project.buildDir.path + '/generated/java'
-        }
-        final String sourceFile
-        if ( null == schemaFile )
-        {
-            sourceFile = sourceDirectory
-        }
-        else
-        {
-            sourceFile = sourceDirectory + System.getProperty( 'file.separator' ) + schemaFile
-        }
+        sourceDirectory = applyDefaultSourceDirectory()
+        destinationDirectory = applyDefaultDestinationDirectory()
+        String sourceFile = createPathToSingleFileOrEntireDirectory(sourceDirectory)
         logger.quiet "Compiling $sourceFile into $destinationDirectory"
+        logger.quiet "Generating classes into $packageName package"
         new File( destinationDirectory ).mkdirs()
-        Exec task = project.tasks.add( 'bob', Exec.class )
+        Exec task = project.tasks.add( 'xjcCommandLine', Exec.class )
         task.commandLine = ['xjc', '-d', destinationDirectory, '-p', packageName, '-xmlschema', '-verbose', '-readOnly', '-mark-generated', sourceFile]
+        def command = task.commandLine.join( ' ' )
+        logger.quiet "Executing $command"
         task.execute()
         project.sourceSets['main'].java.srcDir( destinationDirectory )
+    }
+
+    private createPathToSingleFileOrEntireDirectory(String sourceDirectory) {
+        final String sourceFile
+        if (null == schemaFile) {
+            sourceFile = sourceDirectory
+        } else {
+            sourceFile = sourceDirectory + System.getProperty('file.separator') + schemaFile
+        }
+        return sourceFile
+    }
+
+    private applyDefaultDestinationDirectory() {
+        if (null == destinationDirectory) {
+            destinationDirectory = project.buildDir.path + '/generated/java'
+        }
+        return destinationDirectory
+    }
+
+    private applyDefaultSourceDirectory() {
+        if (null == sourceDirectory) {
+            sourceDirectory = project.sourceSets['main'].resources.srcDirs.toList().first().path
+        }
+        return sourceDirectory
     }
 }
